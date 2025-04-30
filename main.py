5bfad6bfb94e4c5366eccb95132ac24e9f1ef9d8
Turtdle <sam@wanfamily.org>
2025-04-29 22:13:19 -0700

    .

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-
-"""
-
-Goal:
-    1) Write a program (python or java or c++) that solves 3-coloring using
-       the MiniSAT solver
-    2) Input: a graph to be 3-colored
-    3) Output: corresponding boolean formula that can be fed into MiniSAT
-
-
-Example
-
-2 3
-1 3 4
-1 2
-2
-
-represents a graph where:
-
-1) Node 1 is connected to nodes 2 and 3
-2) Node 2 is connected to nodes 1, 3, and 4
-3) Node 3 is connected to nodes 1 and 2
-4) Node 4 is connected to nodes 2
-
-    There are 4 unqiue vertices thus giving us a total of 12 boolean variables
-    4 vertices * 3 colors = 12 boolean variables
-"""
-
 import sys
 
 DEBUG = True
@@ -52,21 +24,18 @@ class Graph_to_CNF():
         num_vertices = len(self.graph)
         clauses = []
         
-        # Function to convert vertex and color to variable index
         def var(vertex, color):
             return (vertex - 1) * 3 + color
         
-        # 1. Each vertex has at least one color
+        
         for vertex in range(1, num_vertices + 1):
             clauses.append([var(vertex, 1), var(vertex, 2), var(vertex, 3)])
         
-        # 2. Each vertex has at most one color
         for vertex in range(1, num_vertices + 1):
             clauses.append([-var(vertex, 1), -var(vertex, 2)])
             clauses.append([-var(vertex, 1), -var(vertex, 3)])
             clauses.append([-var(vertex, 2), -var(vertex, 3)])
         
-        # 3. Adjacent vertices have different colors
         processed_edges = set()
         for vertex in range(1, num_vertices + 1):
             for neighbor in self.graph[vertex]:
@@ -76,7 +45,7 @@ class Graph_to_CNF():
                     for color in range(1, 4):
                         clauses.append([-var(vertex, color), -var(neighbor, color)])
         
-        # Output in DIMACS CNF format
+       
         total_variables = num_vertices * 3
         print(f"p cnf {total_variables} {len(clauses)}")
         for clause in clauses:
@@ -91,7 +60,6 @@ if __name__=="__main__":
     if len(sys.argv) == 1:
         print("Usage: {} [INPUT_FILE]".format(sys.argv[0]))
         exit(0)
-
+    
     graph_to_cnf = Graph_to_CNF(sys.argv[1])
-
-    graph_to_cnf.generatecnf();
+    graph_to_cnf.generatecnf()
